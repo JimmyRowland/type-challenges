2b9d2198760789d7aae39f9dfdc12a5f10997519
ytian <ytian@fortinet.com>
2022-10-25 19:33:06 -0700

    bew

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,4 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer R) => any ? R : never
+
+const foo = (arg1: string, arg2: number): void => {}
+type test = Parameters<typeof foo>
